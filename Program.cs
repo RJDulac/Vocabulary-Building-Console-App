d9d9206184c4c2b1d8ca4f53d2309e5120ca8689
machine <analyticrye@gmail.com>
2021-10-14 12:58:18 -0400

    project started

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VocabularyBuildingConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+
+            do
+            {
+                Console.WriteLine("**Vocabulary Builder***");
+                Console.WriteLine("Press 'Q' to quit.");
+
+                string input = Console.ReadLine().ToUpper();
+                if (input == "Q")
+                {
+                    isRunning == false;
+                }
+
+            } while (isRunning == true);
+        }
+    }
+}
